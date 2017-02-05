8f766c9a4af1024c7ecb66a17566f0cf0c3d1392
James Lucas <LucasJ94@hotmail.co.uk>
2017-02-05 16:26:51 -0500

    Adding tests for arithmetic ops

--- src/functions/op_overrides.rs
+++ src/functions/op_overrides.rs
@@ -42,3 +42,76 @@ impl<E: Expression> ops::Neg for Container<E> {
         Container(Neg(self.0))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use ::{Expression, Gradient, Context};
+    use std::f64;
+
+    #[test]
+    fn test_add_op() {
+        let mut c = Context::new();
+        let x = c.create_variable(1.0);
+        let y = c.create_variable(1.5);
+
+        let f = x + y;
+        assert!((f.eval(&mut c).value - 2.5).abs() < 1e-5);
+        let mut g = Gradient::of(f, c);
+
+        assert!((g.grad(x) - 1.0).abs() < 1e-5);
+        assert!((g.grad(y) - 1.0).abs() < 1e-5);
+    }
+
+    #[test]
+    fn test_mul_op() {
+        let mut c = Context::new();
+        let x = c.create_variable(1.0);
+        let y = c.create_variable(1.5);
+
+        let f = x * y;
+        assert!((f.eval(&mut c).value - 1.5).abs() < 1e-5);
+        let mut g = Gradient::of(f, c);
+
+        assert!((g.grad(x) - 1.5).abs() < 1e-5);
+        assert!((g.grad(y) - 1.0).abs() < 1e-5);
+    }
+
+    #[test]
+    fn test_div_op() {
+        let mut c = Context::new();
+        let x = c.create_variable(1.0);
+        let y = c.create_variable(1.5);
+
+        let f = x / y;
+        assert!((f.eval(&mut c).value - f64::recip(1.5)).abs() < 1e-5);
+        let mut g = Gradient::of(f, c);
+
+        assert!((g.grad(x) - f64::recip(1.5)).abs() < 1e-5);
+        assert!((g.grad(y) + f64::recip(2.25)).abs() < 1e-5);
+    }
+
+    #[test]
+    fn test_sub_op() {
+        let mut c = Context::new();
+        let x = c.create_variable(1.0);
+        let y = c.create_variable(1.5);
+
+        let f = x - y;
+        assert!((f.eval(&mut c).value + 0.5).abs() < 1e-5);
+        let mut g = Gradient::of(f, c);
+
+        assert!((g.grad(x) - 1.0).abs() < 1e-5);
+        assert!((g.grad(y) + 1.0).abs() < 1e-5);
+    }
+
+    #[test]
+    fn test_neg_op() {
+        let mut c = Context::new();
+        let x = c.create_variable(1.5);
+
+        let f = -x;
+        assert!((f.eval(&mut c).value + 1.5).abs() < 1e-5);
+        let mut g = Gradient::of(f, c);
+        assert!((g.grad(x) + 1.0).abs() < 1e-5);
+    }
+}
